2c504101bfe162e5efdbcdb13490f6eb3b4c3814
michael walker <miwalker@atlassian.com>
2020-11-28 01:39:25 -0600

    attempted merge of all items to one place

---
+++ resources/merge.py
@@ -0,0 +1,130 @@
+{'weapons':
+    {'bows':
+        {'amos_bow':
+            {"weapon_material": "dandelion_gladiator",
+            "uncommon_material": "chaos_device",
+            "common_material": "slime_condensate"
+            },
+        'skyward_harp':
+            {"weapon_material": "boreal_wolfs",
+            "uncommon_material": "dead_ley_line_branches",
+            "common_material": "firm_arrowhead"
+            }
+        },
+    'catalysts':
+        {'skyward_atlas':
+            {"weapon_material": "boreal_wolfs",
+            "uncommon_material": "dead_ley_line_branches",
+            "common_material": "firm_arrowhead"
+            },
+        'lost_prayer_to_the_sacred_winds':
+            {"weapon_material": "dandelion_gladiator",
+            "uncommon_material": "chaos_device",
+            "common_material": "slime_condensate"
+            }
+        },
+    'claymores':
+        {'skyward_pride':
+            {"weapon_material": "boreal_wolfs",
+            "uncommon_material": "dead_ley_line_branches",
+            "common_material": "slime_condensate"
+            },
+        'wolfs_gravestone':
+            {"weapon_material": "dandelion_gladiator",
+            "uncommon_material": "chaos_device",
+            "common_material": "divining_scroll"
+            }
+        },
+    'polearms':
+        {'primordial_jade_winged_spear':
+            {"weapon_material": "from_guyun",
+            "uncommon_material": "hunters_sacrificial_knife",
+            "common_material": "recruits_insignia"
+            },
+        'skyward_spine':
+            {"weapon_material": "dandelion_gladiator",
+            "uncommon_material": "chaos_device",
+            "common_material": "divining_scroll"
+            }
+        },
+    'swords':
+        {'aquila_favonia':
+            {"weapon_material": "decarabians",
+            'uncommon_material': 'heavy_horn'
+            'common_material': 'firm_arrowhead'
+            },
+        'skyward_blade':
+            {"weapon_material": "boreal_wolfs",
+            'uncommon_material': 'dead_ley_line_branches',
+            'common_material': 'slime_condensate'
+            }
+        }
+    },
+'materials':
+    {'common':
+        {'damaged_mask': ['damaged_mask', 'stained_mask', 'ominous_mask'],
+        'divining_scroll': ['divining_scroll', 'sealed_scroll', 'forbidden_curse_scroll'],
+        'firm_arrowhead': ['firm_arrowhead', 'sharp_arrowhead', 'weathered_arrowhead'],
+        'recruits_insignia': ['recruits_insignia', 'sergeants_insignia', 'lieutenants_insignia'],
+        'slime_condensate': ['slime_condensate', 'slime_secretions', 'slime_concentrate'],
+        'treasure_hoarder_insignia': ['treasure_hoarder_insignia', 'silver_raven_insignia', 'gloden_raven_insignia'],
+        'whopperflower_nectar': ['whopperflower_nectar', 'shimmering_nectar', 'energy_nectar']
+        },
+    'uncommon':
+        {'heavy_horn': ['heavy_horn', 'black_copper_horn', 'black_crystal_horn'],
+        'dead_ley_line_branches': ['dead_ley_line_branches', 'dead_ley_line_leaves', 'ley_line_sprouts'],
+        'chaos_device': ['chaos_device', 'chaos_circuit', 'chaos_core'],
+        'mist_flower_pollen': ['mist_flower_pollen', 'mist_grass', 'mist_grass_lamp'],
+        'hunters_sacrificial_knife': ['hunters_sacrificial_knife', 'agents_sacrificial_knife', 'inspectors_sacrificial_knife'],
+        'fragile_bone_shard': ['fragile_bone_shard', 'sturdy_bone_shard', 'fossilized_bone_shard']
+        },
+    'rare':
+        {},
+    'talent_books':
+        {},
+    'weapon_materials':
+        {'decarabians': ['tile_of_decarabians_tower', 'debris of decarabians_city', 'fragment_of_decarabians_epic', 'scattered_piece_of_decarabians_dream'],
+        'boreal_wolfs': ['boreal_wolfs_milk_tooth', 'boreal_wolfs_cracked_tooth', 'boreal_wolfs_broken_fang', 'boreal_wolfs_nostalgia'],
+        'dandelion_gladiator': ['fetters_of_the_dandelion_gladiator', 'chains_of_the_dandelion_gladiator', 'shackles_of_the_dandelion_gladiator', 'dream_of_the_dandelion_gladiator'],
+        'from_guyun': ['luminous_sands_from_guyun', 'lustrous_stone_from_guyun', 'relic_from_guyun', 'divine_body_from_guyun'],
+        'mist_veiled': ['mist_veiled_lead_elixir', 'mist_veiled_mercury_elixir', 'mist_veiled_gold_elixir', 'mist_veiled_primo_elixir'],
+        'of_aerosiderite': ['grain_of_aerosiderite', 'piece_of_aersiderite', 'bit_of_aerosiderite', 'chunk_of_aerosiderite']
+        }
+    },
+'elements':
+    {'anemo': 
+        {'core': 'hurricane_seed',
+        'gem': 'vayuda_turquoise'
+        },
+    'cryo': 
+        {'core': 'hoarfrost_core',
+        'gem': 'shivada_jade'
+        },
+    'dendro': 
+        {'core': '',
+        'gem': ''
+        },
+    'electro': 
+        {'core': 'lightning_prism',
+        'gem': 'vajrada_amethyst'
+        },
+    'geo': 
+        {'core': 'basalt_pillar',
+        'gem': 'prithiva_topaz'
+        },
+    'hydro': 
+        {'core': 'cleansing_heart',
+        'gem': 'varunada_lazurite'
+        },
+    'pyro': 
+        {'core': 'everflame_seed',
+        'gem': 'agnidus_agate'
+        },
+    'traveler': 
+        {'core': 'none',
+        'gem': 'brilliant_diamond'
+        },
+    'gem_suffix': {'tier_0': '_sliver', '_fragement', '_chunk', '_gemstone'}
+    },
+'characters':{}
+}
